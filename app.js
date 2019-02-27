68f6f9b9a9ee1f24b355058e5bbca66f1e30fa17
mehechi <maxime1jacquet@gmail.com>
2019-02-27 21:56:10 +0100

    init api

---
+++ app.js
@@ -0,0 +1,158 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const data = require("./projects.js");
+const port = process.env.PORT || 8080;
+const router = express.Router();
+const nodemailer = require('nodemailer');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('./assets'));
+/*
+  LIST ALL PROJECT
+*/
+router.get("/project", (req, res) => {
+  res
+  .header("Access-Control-Allow-Origin", "*")
+  .json({ data: data.projects });
+});
+
+
+/*
+  SERCH BY TITLE
+*/
+router.get("/project/search/:keyword", (req, res) => {
+  let isExist = false;
+  let result = [];
+
+  for (let i = 0; i < data.projects.length; i++) {
+    const title = data.projects[i].title.toLowerCase();
+    const keyword = req.params.keyword.toLowerCase();
+    if (title.indexOf(keyword) !== -1) {
+      isExist = true;
+      result.push(data.projects[i]);
+    }
+  }
+
+  if (!isExist) {
+    res
+      .header("Access-Control-Allow-Origin", "*")
+      .status(404)
+      .send({ status: 404, message: "project not found" });
+  } else {
+    res.header("Access-Control-Allow-Origin", "*").json({ data: result });
+  }
+});
+
+/*
+  SERCH BY SLUG
+*/
+router.get("/project/slug/:keyword", (req, res) => {
+  let isExist = false;
+  let result = [];
+
+  for (let i = 0; i < data.projects.length; i++) {
+    const slug = data.projects[i].slug.toLowerCase();
+    const keyword = req.params.keyword.toLowerCase();
+    if (slug.indexOf(keyword) !== -1) {
+      isExist = true;
+      result.push(data.projects[i]);
+    }
+  }
+
+  if (!isExist) {
+    res
+      .header("Access-Control-Allow-Origin", "*")
+      .status(404)
+      .send({ status: 404, message: "project not found" });
+  } else {
+    res.header("Access-Control-Allow-Origin", "*").json({ data: result });
+  }
+});
+
+/*
+  TAGS
+*/
+router.get("/project/tag", (req, res) => {
+  let tags = [];
+
+  for (let i = 0; i < data.projects.length; i++) {
+    const slug = data.projects[i].tag.toLowerCase().split(",");
+    tags.push(...slug);
+  }
+
+  tags = tags.reduce(function(a, b) {
+    if (a.indexOf(b) < 0) a.push(b);
+    return a;
+  }, []);
+
+  res.header("Access-Control-Allow-Origin", "*").json({ tags: tags });
+});
+
+/*
+  SERCH BY ID
+router.get("/project/:id", (req, res) => {
+  let isExist = false;
+  for (let i = 0; i < data.projects.length; i++) {
+    if (data.projects[i].id == req.params.id) {
+      isExist = true;
+      res
+      .header('Access-Control-Allow-Origin', "*")
+      .json({ data: data.projects[i] });
+    }
+  }
+
+  if (!isExist) {
+    res
+    .header('Access-Control-Allow-Origin', "*")
+    .status(404)
+    .send({ status: 404, message: "project not found" });
+  }
+});
+*/
+
+/*
+  TAGS
+*/
+router.get("/mail", (req, res) => {
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main(){
+
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let account = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'maxime1jacquet@gmail.com', // generated ethereal user
+      pass: 'gMehechi7l' // generated ethereal password
+    }
+  });
+
+  let mailOptions = {
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "maxime1jacquet@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>" // html body
+  };
+
+  let info = await transporter.sendMail(mailOptions)
+
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+}
+
+main().catch(console.error);
+});
+
+app.use("/", router);
+app.listen(port);
+console.log("Listening " + port);
---
+++ projects.js
@@ -0,0 +1,182 @@
+module.exports = {
+  "projects" : [
+    {
+      "id": 10,
+      "slug": "roquette-frere",
+      "title": "Roquette Frère",
+      "date": "12/2018",
+      "tag": "VUEJS,HTML,SASS",
+      "text": "En entreprise, je participe à la réalisation de la partie front-end du site Roquette frère.",
+      "porfolio": [
+        {
+          "type": "texte",
+          "texte": "En entreprise, je participe à la réalisation de la partie front-end du site Roquette frère. Un projet important qui nous oblige à nous concentré sur la réalisation du projet. Maintenabilité, qualité de code, accessibilité et compatibilité. Sur ce site nous avons créé plusieurs applications avec Vue.Js ces applications permettent le rechargement en AJAX des produits ou des résultats de recherche."
+        },
+        {
+          "type": "site",
+          "label": "Application de recherche",
+          "url": "https://www.roquette.com/site-search"
+        },
+        {
+          "type": "site",
+          "label": "Voir le product finder",
+          "url": "https://www.roquette.com/product-finder-pharma-and-nutraceuticals/"
+        },
+        {
+          "type": "image",
+          "lien": "1.jpg"
+        }
+      ]
+    },
+    {
+      "id": 11,
+      "slug": "m-comme-mutuelle",
+      "title": "M comme Mutuelle",
+      "date": "03/2018",
+      "tag": "ANGULAR,HTML,SASS",
+      "text": "Réalisation d'une digital workplace sur Angular.",
+      "porfolio": [
+        {
+          "type": "texte",
+          "texte": "En entreprise, je participe à la réalisation de la partie front-end du site Roquette frère. Un projet important qui nous oblige à nous concentré sur la réalisation du projet. Maintenabilité, qualité de code, accessibilité et compatibilité. Sur ce site nous avons créé plusieurs applications avec Vue.Js ces applications permettent le rechargement en AJAX des produits ou des résultats de recherche."
+        },
+        {
+          "type": "site",
+          "label": "Application de recherche",
+          "url": "https://www.roquette.com/site-search"
+        },
+        {
+          "type": "site",
+          "label": "Voir le product finder",
+          "url": "https://www.roquette.com/product-finder-pharma-and-nutraceuticals/"
+        },
+        {
+          "type": "image",
+          "lien": "1.jpg"
+        }
+      ]
+    },
+    {
+      "id": 9,
+      "slug": "threejs-experiment",
+      "title": "ThreeJS Experiment",
+      "date": "01/2018",
+      "tag": "THREEJS,VUEJS",
+      "text": "Pour apprendre et par curiosité j'ai décidé de créer ma première expérience en javascript avec la librairie : Three.js.",
+      "porfolio": [
+        {
+          "type": "texte",
+          "texte": "Pour apprendre et par curiosité j'ai décidé de créer ma première expérience en javascript avec la librairie : Three.js. Elle consiste à voyager dans le vide de l’espace autour de la terre et du soleil."
+        },
+        {
+          "type": "site",
+          "label": "Voir le site",
+          "url": "https://experiment.maximejacquet.fr/"
+        },
+        {
+          "type": "image",
+          "lien": "1.jpg"
+        }
+      ]
+    },
+    {
+      "id": 8,
+      "slug": "auto-ecole-laurent",
+      "title": "Auto-école Laurent",
+      "date": "04/2018",
+      "tag": "HTML,SASS,WORDPRESS",
+      "text": "Pour une nouvelle auto-école j'ai réalisé un plan de communication contenant logo, carte de visite et site internet.",
+      "porfolio": [
+        {
+          "type": "texte",
+          "texte": "Pour une nouvelle auto-école j'ai réalisé un plan de communication contenant logo, carte de visite et site internet. Le but du site et de donner le maximum d’informations aux personnes cherchant à passer leur permis de conduire. Pour progresser en framework javascript j’ai choisi d’utiliser vue.js. Ce framework me permettra de mettre en place des transitions au chargement du site et entre les pages."
+        },
+        {
+          "type": "site",
+          "label": "Voir le site",
+          "url": "https://autoecole-laurent.fr"
+        },
+        {
+          "type": "image",
+          "lien": "1.png"
+        },
+        {
+          "type": "image",
+          "lien": "2.jpg"
+        }
+      ]
+    },
+    {
+      "id": 7,
+      "slug": "agence-web-ariane",
+      "title": "Agence web Ariane",
+      "date": "11/2017",
+      "tag": "HTML,CSS,WORDPRESS,SEO",
+      "text": "En groupe de 6, nous avons crée un thème WordPress pour la réalisation d’un site d’agence web.",
+      "porfolio": [
+        {
+          "type": "texte",
+          "texte": "En groupe de 6, nous avons crée un thème WordPress pour la réalisation d’un site d’agence web. Le but du site est de créér du contenu et d’optimiser le référencement naturel. Grâce à Webpack et nodeJS nous avons crée un thème wordpress optimisé responsive."
+        },
+        {
+          "type": "site",
+          "label": "Voir le site",
+          "url": "http://agence-ariane.fr/"
+        },
+        {
+          "type": "image",
+          "lien": "1.png"
+        },
+        {
+          "type": "texte",
+          "texte": "Pour optimiser le référencement naturel de notre agence fictive nous avons : Pensé notre arborescence, Optimisé les balises Titles & métas descriptions, Optimisé les balises H1, H2 & H3., Ajouté du contenu en lien avec notre activité, Ajouté des liens internes, Optimisé les ancres de liens, Configuré la Google Search Console, Ajouté des backlinks"
+        },
+        {
+          "type": "image",
+          "lien": "2.jpg"
+        },
+        {
+          "type": "texte",
+          "texte": "Voici notre positionnement sur la requête google : Agence Web Lens. Notre fausse agence à reçu plus de 10 demandes de stage et 2 demandes de partenariat"
+        },
+        {
+          "type": "image",
+          "lien": "3.jpg"
+        }
+      ]
+    },
+    {
+      "id": 6,
+      "slug": "court-metrage-carlzone",
+      "title": "Court-métrage Carlzone",
+      "date": "03/2017",
+      "tag": "PRINT,VIDEO",
+      "text": "Avec mon groupe, nous avons réalisé un court-métrage de 10min.",
+      "porfolio": [
+        {
+          "type": "texte",
+          "texte": "Avec mon groupe, nous avons réalisé un court-métrage de 10min. Mon rôle de pré-production a été d’imaginer les plans que comporterait CarlZone. Le découpage technique est le document sur lequel s’est appuyé le réalisateur pendant le tournage du film. Durant le tournage, j’étais le directeur photographie. Je me suis occupé de mettre en place l’ambiance que notre réalisateur avait imaginé. Et enfin, j’ai en post-production conçu générique « original » de fin. A découvrir à la fin du film !"
+        },
+        {
+          "type": "site",
+          "label": "Voir le film",
+          "url": "https://www.youtube.com/watch?v=hqTTYSuiLj0"
+        },
+        {
+          "type": "site",
+          "label": "Voir la bande annonce",
+          "url": "https://www.youtube.com/watch?v=HbWviGpnHiw"
+        },
+        {
+          "type": "site",
+          "label": "Voir le making of",
+          "url": "https://www.youtube.com/watch?v=V2Rhnx_Enw0"
+        },
+        {
+          "type": "image",
+          "lien": "1.jpg"
+        }
+      ]
+    }
+  ]
+}
